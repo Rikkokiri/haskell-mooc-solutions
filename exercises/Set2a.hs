acd51a0cdeba2c6602d5f7ac8d8dea6c7999a590
Ossian Rajala <ossian.rajala@gmail.com>
2020-09-06 21:12:18 +0300

    Solve exercise set 2a

--- exercises/Set2a.hs
+++ exercises/Set2a.hs
@@ -32,7 +32,7 @@ years = [1982, 2004, 2020]
 -- Hint! remember the take and drop functions.
 
 takeFinal :: Int -> [a] -> [a]
-takeFinal n xs = todo
+takeFinal n xs = drop (length xs - n) xs
 
 ------------------------------------------------------------------------------
 -- Ex 3: Update an element at a certain index in a list. More
@@ -46,7 +46,10 @@ takeFinal n xs = todo
 --   updateAt 2 0 [4,5,6,7] ==>  [4,5,0,7]
 
 updateAt :: Int -> a -> [a] -> [a]
-updateAt i x xs = todo
+updateAt i x xs
+    | i == 0                = [x] ++ tail xs
+    | i == length xs -1     = take i xs ++ [x]
+    | otherwise             = take i xs ++ [x] ++ drop (i+1) xs
 
 ------------------------------------------------------------------------------
 -- Ex 4: substring i j s should return the substring of s starting at
@@ -60,7 +63,10 @@ updateAt i x xs = todo
 --   substring 0 4 "abcdefgh"  ==>  "abcd"
 
 substring :: Int -> Int -> String -> String
-substring i j s = todo
+substring i j s = take (j-i) (drop i s)
+
+-- take :: Int -> [a] -> [a]   -- returns the n first elements
+-- drop :: Int -> [a] -> [a]   -- returns everything except the n first elements
 
 ------------------------------------------------------------------------------
 -- Ex 5: check if a string is a palindrome. A palindrome is a string
@@ -89,7 +95,9 @@ isPalindrome str = str == reverse str
 --   palindromify "abracacabra" ==> "acaca"
 
 palindromify :: String -> String
-palindromify s = todo
+palindromify s
+    | isPalindrome s    = s
+    | otherwise         = palindromify (tail (init s ))
 
 ------------------------------------------------------------------------------
 -- Ex 7: implement safe integer division, that is, a function that
@@ -114,7 +122,7 @@ safeDiv x y = if y == 0 then Nothing else Just (x `div` y)
 --   greet "John" (Just "Smith")  ==> "Hello, John Smith!"
 
 greet :: String -> Maybe String -> String
-greet first last = "Hello, " ++ first ++ " " ++ last ++ "!"
+greet first (Just last) = "Hello, " ++ first ++ " " ++ last ++ "!"
 greet first Nothing = "Hello, " ++ first ++ "!"
 
 ------------------------------------------------------------------------------
@@ -131,7 +139,10 @@ greet first Nothing = "Hello, " ++ first ++ "!"
 --   safeIndex ["a","b","c"] (-1)  ==> Nothing
 
 safeIndex :: [a] -> Int -> Maybe a
-safeIndex xs i = todo
+safeIndex xs i
+    | i < 0             = Nothing
+    | i >= length xs    = Nothing
+    | otherwise         = Just (xs !! i)
 
 ------------------------------------------------------------------------------
 -- Ex 10: another variant of safe division. This time you should use
@@ -142,7 +153,8 @@ safeIndex xs i = todo
 --   eitherDiv 4 0   ==> Left "4/0"
 
 eitherDiv :: Integer -> Integer -> Either String Integer
-eitherDiv x y = todo
+eitherDiv x 0 = Left (show x ++ "/0")
+eitherDiv x y = Right (x `div` y)
 
 ------------------------------------------------------------------------------
 -- Ex 11: implement the function addEithers, which combines two values of type
@@ -160,4 +172,7 @@ eitherDiv x y = todo
 -- addEithers (Left "boom") (Left "fail") ==> Left "boom"
 
 addEithers :: Either String Int -> Either String Int -> Either String Int
-addEithers a b = todo
+addEithers (Right a) (Right b) = Right (a + b)
+addEithers (Right a) (Left b) = Left b
+addEithers (Left a) (Right b) = Left a
+addEithers (Left a) (Left b) = Left a
